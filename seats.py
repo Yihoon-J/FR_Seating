8d3e67e5aa62ff62bcf5583df5c429b102ceb661
Yihoon Jeon <96404152+Yihoon-J@users.noreply.github.com>
2023-09-13 19:56:46 +0900

    add sys.argv for selecting rows

--- seats.py
+++ seats.py
@@ -1,13 +1,14 @@
 import random
 import numpy as np
 import time
+import sys
 students=['이대원','김동현','송승호','장은경','조석현','서자빈','이창기','전호연','정상훈',\
     '전이훈','강석희','권세종','김명주','이소현','박우석','정  웅','오성준','이대현','이석연',\
     '전제민','김상혁','성민혁','김수현','정지훈','이나연']
 
 random.shuffle(students)
 
-row=6
+row=int(sys.argv[1])
 
 if row==6: #6*5:
     students+=[None]*(30-len(students))
